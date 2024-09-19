cffe7c58ba8d499528cad2d3e244c9e41ad6f6dc
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-09-19 15:21:18 +0300

    Add solution to coin change problem

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,11 +8,11 @@ from typing import List
 def makeChange(coins: List[int], total: int) -> int:
     """return the fewest number of coins needed to meet the total
     """
-    dp = [total + 1] * (total + 1)
-    dp[0] = 0
+    def coinChange(self, coins, amount):
+        MAX = float('inf')
+        dp = [0] + [MAX] * amount
 
-    for a in range(1, total + 1):
-        for c in coins:
-            if a - c >= 0:
-                dp[a] = min(dp[a], 1 + dp[a - c])
-    return dp[total] if dp[total] != total + 1 else -1
+        for i in xrange(1, amount + 1):
+            dp[i] = min([dp[i - c] if i - c >= 0 else MAX for c in coins]) + 1
+
+        return [dp[amount], -1][dp[amount] == MAX]
