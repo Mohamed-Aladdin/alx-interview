8ca0204da16b30bf11cd5bf6693f16cfc7201b2b
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-09-24 14:14:53 +0300

    Fix pycodestyle warnings

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,25 +6,18 @@
 def island_perimeter(grid):
     """Returns the perimeter of the island described in grid
     """
-    checked = set()
-
-    def dfs(i, j):
-        """Depth first search algorithm
-        """
-        if i >= len(grid) or j >= len(grid[0]) or i < 0 or j < 0 \
-                or grid[i][j] == 0:
-            return 1
-        if (i, j) in checked:
-            return 0
-        checked.add((i, j))
-        perimeter = dfs(i, j + 1)
-        perimeter += dfs(i + 1, j)
-        perimeter += dfs(i, j - 1)
-        perimeter += dfs(i - 1, j)
-
-        return perimeter
+    perimeter = 0
 
     for i in range(len(grid)):
         for j in range(len(grid[0])):
-            if grid[i][j]:
-                return dfs(i, j)
+            if grid[i][j] == 1:
+                if i == 0 or grid[i-1][j] == 0:
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i+1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j-1] == 0:
+                    perimeter += 1
+                if j == len(grid[0]) - 1 or grid[i][j+1] == 0:
+                    perimeter += 1
+
+    return perimeter
