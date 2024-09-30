c2e5f7e253b7d9ef16037f670d5387122c27da83
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-09-30 14:04:40 +0300

    Initial approach

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Prime Game Module"""
+
+def isWinner(x, nums):
+    """Return the name of the player that won the most rounds
+    """
+    maria, ben = 0
+
+    for num in nums:
+        if num < 2:
+            ben += 1
+        else:
+            primes = 0
+
+            for i in range(3, num + 1):
+                if i == 2 or i == 3:
+                    primes += 1
+                    continue
+                if i % 2 != 0 and i % 3 != 0:
+                    primes += 1
+            if primes % 2 == 0:
+                ben += 1
+            else:
+                maria += 1
+    if ben > maria:
+        return "Ben"
+    elif ben >= maria:
+        return None
+    else:
+        return "Maria"
+
+
+if __name__ == "__name__":
+    print(isWinner(3, [4, 5, 1]))
