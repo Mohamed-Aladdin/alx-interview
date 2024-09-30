00829b7dfd87401d685757775d4972a874128476
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-09-30 14:37:05 +0300

    Fix pycodestyle warnings

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -16,10 +16,10 @@ def isWinner(x, nums):
             primes = 0
 
             for i in range(2, num + 1):
-                if i == 2 or i == 3:
+                if i == 2 or i == 3 or i == 5:
                     primes += 1
                     continue
-                if i % 2 != 0 and i % 3 != 0:
+                if i % 2 != 0 and i % 3 != 0 and i % 5 != 0:
                     primes += 1
             if primes % 2 == 0:
                 ben += 1
